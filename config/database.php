aba61338f94e15b58a9ba1e3c497a0c763919388
alideveloper11 <96301770+alideveloper11@users.noreply.github.com>
2026-05-18 14:09:24 +0500

    Refactor PDO MySQL options in database config
    Simplified the options array for PDO MySQL configuration.

--- config/database.php
+++ config/database.php
@@ -59,17 +59,13 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-                       'options' => extension_loaded('pdo_mysql') ? array_merge(
-                array_filter([
-                    (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                ]),
-                [
-                    (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_VERIFY_SERVER_CERT : PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => filter_var(
-                        env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', 'false'),
-                        FILTER_VALIDATE_BOOLEAN
-                    ),
-                ],
-            ) : [],
+                       'options' => extension_loaded('pdo_mysql')
+    ? array_filter([
+        (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+    ]) + [
+        (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_VERIFY_SERVER_CERT : PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => false,
+    ]
+    : [],
         ],
 
         'mariadb' => [
