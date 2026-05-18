53074181ba2fc2b30ae407dd71586ae3f82135ba
alideveloper11 <96301770+alideveloper11@users.noreply.github.com>
2026-05-18 14:05:04 +0500

    Enhance SSL options in database configuration

--- config/database.php
+++ config/database.php
@@ -87,9 +87,18 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql') ? array_merge(
+    array_filter([
+        (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+    ]),
+    [
+        (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_VERIFY_SERVER_CERT : PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => in_array(
+            strtolower((string) env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', 'false')),
+            ['1', 'true', 'yes', 'on'],
+            true
+        ),
+    ],
+) : [],
         ],
 
         'pgsql' => [
